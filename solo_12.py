52ff428fe97b0a08dd9316e160d86401609bf1f3
DimasWerito <krasilnykov@gmail.com>
2024-02-24 16:40:23 -0600

    solo beginner

---
+++ solo_12.py
@@ -0,0 +1,22 @@
+"""
+You are creating your own social network and need to turn words into hashtags.
+
+The code takes a word as input.
+
+ 
+
+Task
+
+Complete the hashtag() function to return the input word starting with the hashtag symbol (#). Then display the result on the screen.
+"""
+
+
+#taking a word as input
+word = input()
+
+def hashtag(word):
+    #complete the function body
+    return "#" + word
+
+#display the result
+print(hashtag(word))
